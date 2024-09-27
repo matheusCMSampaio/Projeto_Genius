4eb9dfa106b9337db9db3f73085b911019010708
Matheus Sampaio <86751496+matheusCMSampaio@users.noreply.github.com>
2024-09-26 22:49:43 -0300

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,7 +1,10 @@
+String recebido;
+String sequenciaNumerica = "";
+const long SERIAL_TIMEOUT = 10000;
+
 enum GameState {
   START_GAME,
-  LEVEL_1,
-  LEVEL_2,
+  PLAY_GAME,
   GAME_OVER,
   PLAY_AGAIN
 };
@@ -21,7 +24,6 @@ void setup() {
   }
   randomSeed(analogRead(0));
   
-  // Configurando os botões como INPUT_PULLUP
   pinMode(botaoVerde, INPUT_PULLUP);
   pinMode(botaoAmarelo, INPUT_PULLUP);
   pinMode(botaoAzul, INPUT_PULLUP);
@@ -30,11 +32,11 @@ void setup() {
   Serial.begin(9600);
 }
 
-void piscaled(int tempo, int vezes) {
+void piscaled(int vezes) {
   for (int i = 0; i < vezes; i++) {
     for (int j = 0; j < NUM_LEDS; j++) {
       digitalWrite(LED_PINS[j], HIGH);
-      delay(200);
+      delay(150);
       digitalWrite(LED_PINS[j], LOW);
     }
   }
@@ -49,12 +51,78 @@ void loop() {
         delay(50); 
         if (digitalRead(botaoVerde) == LOW || digitalRead(botaoVermelho) == LOW || 
             digitalRead(botaoAzul) == LOW || digitalRead(botaoAmarelo) == LOW) {
-          piscaled(200, 5);
+          piscaled(4);
+          stateGame = PLAY_GAME;
           delay(1000);
         }
       }
       break;
+    case PLAY_GAME:
+      geraSequencia(400, 2);
+      leBotao();
+      break;
 
-    
   }
+}
+
+
+void geraSequencia (int tempo, int sequencia){
+  int ordemLeds[sequencia];
+
+  for (int i = 0; i < sequencia; i++){
+    ordemLeds[i] = random(1, NUM_LEDS + 1);
+  }
+
+  sequenciaNumerica = "";
+
+  for (int j = 0; j < sequencia; j++){
+    int ledIndex = ordemLeds[j] - 1;
+    
+    digitalWrite(LED_PINS[ledIndex], HIGH);
+    delay(tempo);
+    digitalWrite(LED_PINS[ledIndex], LOW);
+    delay(tempo);
+    sequenciaNumerica += String(ordemLeds[j]);
+  }
+  Serial.println(sequenciaNumerica);
+  delay(5000);
+}
+
+void leBotao(){
+  
+  long startTime = millis();
+  while (millis() - startTime < SERIAL_TIMEOUT){
+    if (digitalRead(botaoVerde) == LOW) {
+      delay(50); 
+      if (digitalRead(botaoVerde) == LOW) {
+        recebido += "1";  
+         
+      }
+    }
+    else if (digitalRead(botaoAmarelo) == LOW) {
+      delay(50); 
+      if (digitalRead(botaoAmarelo) == LOW) {
+        recebido += "2";  
+        
+      }
+    }
+    else if (digitalRead(botaoAzul) == LOW) {
+      delay(50); 
+      if (digitalRead(botaoAzul) == LOW) {
+        recebido += "3";  
+        
+      }
+    }
+    else if (digitalRead(botaoVermelho) == LOW) {
+      delay(50); // debounce
+      if (digitalRead(botaoVermelho) == LOW) {
+        recebido += "4";  
+      }
+    }
+    if (millis() - startTime >= SERIAL_TIMEOUT) {
+      break;
+    }
+  }
+
+  Serial.println(recebido);
 }
