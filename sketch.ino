6514b9b9a07796387accae479f57c927988c8121
Matheus Sampaio <86751496+matheusCMSampaio@users.noreply.github.com>
2024-10-04 11:43:43 -0300

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -17,6 +17,7 @@ const int botaoAmarelo = 11;
 const int botaoAzul = 2;    
 const int botaoVerde = 3;  
 const int botaoVermelho = 4; 
+const int buzzerPin = 13;  // Pino do buzzer
 
 void setup() {
   for (int i = 0; i < NUM_LEDS; i++) {
@@ -28,6 +29,7 @@ void setup() {
   pinMode(botaoAmarelo, INPUT_PULLUP);
   pinMode(botaoAzul, INPUT_PULLUP);
   pinMode(botaoVermelho, INPUT_PULLUP);
+  pinMode(buzzerPin, OUTPUT); // Configura o pino do buzzer como saída
   
   Serial.begin(9600);
 }
@@ -45,7 +47,6 @@ void piscaled(int vezes) {
 void loop() {
   switch (stateGame) {
     case START_GAME:
-      
       if (digitalRead(botaoVerde) == LOW || digitalRead(botaoVermelho) == LOW || 
           digitalRead(botaoAzul) == LOW || digitalRead(botaoAmarelo) == LOW) {
         delay(50); 
@@ -57,6 +58,7 @@ void loop() {
         }
       }
       break;
+
     case PLAY_GAME:
       recebido = "";
       sequenciaNumerica = "";
@@ -64,21 +66,19 @@ void loop() {
       leBotao();
       SERIAL_TIMEOUT = 6000;
       break;
-
   }
 }
 
-
-void geraSequencia (int tempo, int sequencia){
+void geraSequencia(int tempo, int sequencia) {
   int ordemLeds[sequencia];
 
-  for (int i = 0; i < sequencia; i++){
+  for (int i = 0; i < sequencia; i++) {
     ordemLeds[i] = random(1, NUM_LEDS + 1);
   }
 
   sequenciaNumerica = "";
 
-  for (int j = 0; j < sequencia; j++){
+  for (int j = 0; j < sequencia; j++) {
     int ledIndex = ordemLeds[j] - 1;
     
     digitalWrite(LED_PINS[ledIndex], HIGH);
@@ -86,37 +86,32 @@ void geraSequencia (int tempo, int sequencia){
     digitalWrite(LED_PINS[ledIndex], LOW);
     delay(tempo);
     sequenciaNumerica += String(ordemLeds[j]);
-
   }
-  
 }
 
-void leBotao(){
+void leBotao() {
   long startTime = millis();
-  while (millis() - startTime < SERIAL_TIMEOUT){
-
+  while (millis() - startTime < SERIAL_TIMEOUT) {
     if (digitalRead(botaoVerde) == LOW) {
-      delay(100); 
+      delay(150);
       if (digitalRead(botaoVerde) == LOW) {
         recebido += "3";
         acendeLed(2);
-        //delay(100); 
+        tocarSom(3); // Toca som para o botão verde
         SERIAL_TIMEOUT += 5000;
-        if(verificaRecebido(recebido)){
+        if (verificaRecebido(recebido)) {
           stateGame = START_GAME;
           break;
         }
       }
-    }
-    else if (digitalRead(botaoAmarelo) == LOW) {
-      delay(100); 
+    } else if (digitalRead(botaoAmarelo) == LOW) {
+      delay(150);
       if (digitalRead(botaoAmarelo) == LOW) {
-        recebido += "1";  
+        recebido += "1";
         acendeLed(0);
-        //delay(100); 
-
+        tocarSom(1); // Toca som para o botão amarelo
         SERIAL_TIMEOUT += 5000;
-        if(verificaRecebido(recebido)){
+        if (verificaRecebido(recebido)) {
           stateGame = START_GAME;
           break;
         }
@@ -124,23 +119,24 @@ void leBotao(){
     }
 
     if (digitalRead(botaoAzul) == LOW) {
-      delay(100); 
+      delay(150);
       if (digitalRead(botaoAzul) == LOW) {
         recebido += "2";
-        acendeLed(1);  
-        //delay(100); 
+        acendeLed(1);
+        tocarSom(2); // Toca som para o botão azul
         SERIAL_TIMEOUT += 5000;
-        if(verificaRecebido(recebido)){
+        if (verificaRecebido(recebido)) {
           break;
         }
       }
-    }else if (digitalRead(botaoVermelho) == LOW) {
-      delay(100); 
+    } else if (digitalRead(botaoVermelho) == LOW) {
+      delay(150);
       if (digitalRead(botaoVermelho) == LOW) {
         recebido += "4";
         acendeLed(3);
+        tocarSom(4); // Toca som para o botão vermelho
         SERIAL_TIMEOUT += 5000;
-        if(verificaRecebido(recebido)){
+        if (verificaRecebido(recebido)) {
           stateGame = START_GAME;
           break;
         }
@@ -149,17 +145,16 @@ void leBotao(){
     if (millis() - startTime >= SERIAL_TIMEOUT) {
       break;
     }
-    
   }
 }
 
-boolean verificaRecebido(String recebido){
-  if(recebido.equals(sequenciaNumerica)){
+boolean verificaRecebido(String recebido) {
+  if (recebido.equals(sequenciaNumerica)) {
     stateGame = START_GAME;
     return true;
-  }else{
-    for(int i = 0; i < recebido.length(); i++){
-      if(recebido.charAt(i) != sequenciaNumerica.charAt(i)){
+  } else {
+    for (int i = 0; i < recebido.length(); i++) {
+      if (recebido.charAt(i) != sequenciaNumerica.charAt(i)) {
         stateGame = START_GAME;
         return true;
       }
@@ -168,12 +163,17 @@ boolean verificaRecebido(String recebido){
   }
 }
 
-void acendeLed(int led){
+void acendeLed(int led) {
   Serial.println(led);
   digitalWrite(LED_PINS[led], HIGH);
   Serial.println("AQUI");
   delay(100);
   digitalWrite(LED_PINS[led], LOW);
+}
 
-} 
-
+void tocarSom(int nota) {
+  int frequencias[] = {262, 294, 330, 349}; 
+  if (nota >= 1 && nota <= 4) {
+    tone(buzzerPin, frequencias[nota - 1], 200); // Toca a nota por 200ms
+  }
+}
